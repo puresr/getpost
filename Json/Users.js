fecf8aeb7f0666ad17bb21db9bcdcc806be84647
puresr <72736641+puresr@users.noreply.github.com>
2021-06-17 08:47:28 +0700

    Add files via upload

---
+++ Json/Users.js
@@ -0,0 +1,14 @@
+const users = [
+    {
+      id: '1',
+      name: 'Saran',
+      email: 'saran@gmail.com'  
+    },
+    {
+        id: '2',
+        name: 'Pure',
+        email: 'pure@gmail.com'  
+      }
+]
+
+module.exports = users;
